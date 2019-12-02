79fbdaefea2c99d020f63c3fc012be7f234a0fcb
Phil <requistp@gmail.com>
2019-12-02 14:14:54 -0800

    excellent progress

--- W2F/Systems/ControllerSystem.fs
+++ W2F/Systems/ControllerSystem.fs
@@ -5,77 +5,89 @@ open Component
 open ComponentEnums
 open ControllerComponent
 open Entities
+open System
 
-let private getCurrentActions (ent:Entities) (actions:ActionTypes[]) (entityID:EntityID) (round:RoundNumber) = 
+let private getCurrentActions (ent:Entities) (round:RoundNumber) (c:ControllerComponent) = 
     //let movesAllowed = MovementActionsAllowed enm entityID
-    actions 
-    |> Array.choose (fun a -> //actionEnabledTest
-        match a with
-        | Eat -> Some Eat //if EatActionEnabled enm entityID then Some Eat else None
-        | Idle -> Some Idle
-        | Mate -> Some Mate //if MateActionEnabled enm entityID round then Some Mate else None
-        | Move_North -> Some Move_North //if Array.contains Move_North movesAllowed then Some Move_North else None
-        | Move_East ->  Some Move_East //if Array.contains Move_East  movesAllowed then Some Move_East  else None
-        | Move_South -> Some Move_South //if Array.contains Move_South movesAllowed then Some Move_South else None
-        | Move_West ->  Some Move_West //if Array.contains Move_West  movesAllowed then Some Move_West  else None
-        )
+    let newCurrentActions =
+        c.CurrentActions 
+        |> Array.choose (fun a -> //actionEnabledTest
+            match a with
+            | Eat -> Some Eat //if EatActionEnabled enm entityID then Some Eat else None
+            | Idle -> Some Idle
+            | Mate -> Some Mate //if MateActionEnabled enm entityID round then Some Mate else None
+            | Move_North -> Some Move_North //if Array.contains Move_North movesAllowed then Some Move_North else None
+            | Move_East ->  Some Move_East //if Array.contains Move_East  movesAllowed then Some Move_East  else None
+            | Move_South -> Some Move_South //if Array.contains Move_South movesAllowed then Some Move_South else None
+            | Move_West ->  Some Move_West //if Array.contains Move_West  movesAllowed then Some Move_West  else None
+            )
+    match (arrayContentsMatch newCurrentActions c.CurrentActions) with
+    | true -> c
+    | false -> { c with CurrentActions = newCurrentActions }
 
 let getInputForAllEntities (ent:Entities) (*log:agent_GameLog*) (round:RoundNumber) (*renderer:(EntityManager->EntityID->unit) option*) = 
-    let setCurrentActions (c:ControllerComponent) = 
-        let newCurrent = getCurrentActions ent c.PotentialActions c.EntityID round
-        match (arrayContentsMatch newCurrent c.CurrentActions) with
-        | true -> c
-        | false -> { c with CurrentActions = newCurrent }
-
-    let getAIInputForEntity (cc:ControllerComponent) =
+    let getActionForEntity (cc:ControllerComponent) =
+        //let awaitKeyboardInput (*renderer:(EntityManager->EntityID->unit) option*) : ActionTypes =
+        //    let mutable _action = None            
+        //    // Uncomment for Entity-view... 
+        //    //if renderer.IsSome then
+        //    //    Async.Parallel
+        //    //    (
+        //    //        renderer.Value enm (controller.EntityID)
+        //    //    )        
+        //    let handleKeyPressed (k:ConsoleKeyInfo) = 
+        //        while Console.KeyAvailable do //Might help clear double movement keys entered in one turn
+        //            Console.ReadKey(true).Key |> ignore        
+        //        match k.Key with 
+        //        | ConsoleKey.Escape -> Some (Idle,false)
+        //        | ConsoleKey.Spacebar -> Some (Idle,true)
+        //        | ConsoleKey.E -> if actionIsAllowed cc Eat  then Some (Eat,true) else None
+        //        | ConsoleKey.M -> if actionIsAllowed cc Mate then Some (Mate,true) else None
+        //        | ConsoleKey.RightArrow -> if actionIsAllowed cc Move_East  then Some (Move_East,true) else None
+        //        | ConsoleKey.UpArrow    -> if actionIsAllowed cc Move_North then Some (Move_North,true) else None
+        //        | ConsoleKey.DownArrow  -> if actionIsAllowed cc Move_South then Some (Move_South,true) else None
+        //        | ConsoleKey.LeftArrow  -> if actionIsAllowed cc Move_West  then Some (Move_West,true) else None
+        //        | _ -> None
+        //    while _action.IsNone do
+        //        while not Console.KeyAvailable do
+        //            System.Threading.Thread.Sleep 2
+        //        _action <- handleKeyPressed (Console.ReadKey(true))        
+        //    _action.Value
         {
             cc with
                 CurrentAction = 
                     match cc.ControllerType with
                     | AI_Random -> 
                         cc.CurrentActions.[random.Next(cc.CurrentActions.Length)]
-                    | _ -> Idle // Should raise an error?
-        }
-
-    let getKeyboardInputForEntity (cc:ControllerComponent) =
-        {
-            cc with
-                CurrentAction = 
-                    match cc.ControllerType with
                     | Keyboard -> 
-                        awaitKeyboardInput enm controller renderer round
+                        cc.CurrentActions.[random.Next(cc.CurrentActions.Length)]
+                        //awaitKeyboardInput 
                     | _ -> Idle // Should raise an error?
         }
-    //    let newAction,cont =
-    //        match controller.ControllerType with
-    //        | Keyboard -> 
-    //            AwaitKeyboardInput enm controller renderer round
-    //        | _ -> Idle,false // Should raise an error
-    //    match cont with
-    //    | false -> false
-    //    | true -> 
-    //        updateActionIfChanged controller newAction
-    //        //if (newAction <> controller.CurrentAction) then
-    //        //    enm.UpdateComponent (Controller { controller with CurrentAction = newAction })
-    //        //    log.Log round (sprintf "%-3s | %-20s -> %-30s #%7i.%i : %A" "Ok" "Controller System" "Current action" controller.EntityID.ToUint32 controller.ID.ToUint32 newAction)
-    //        true
-
+    let handleSplitInputTypes (keyboard:ControllerComponent[], ai:ControllerComponent[]) =
+        ai 
+        |> Array.Parallel.map getActionForEntity
+        |> Array.append (
+            keyboard 
+            |> Array.map getActionForEntity)
     //start
     ControllerComponent
     |> getComponents_OfType ent
-    |> Array.map (fun (Controller c) -> setCurrentActions c) 
-    |> Array.fold (fun (m:Map<ComponentID,Component>) (cc:ControllerComponent) ->
-        let newCC = 
-            match cc.ControllerType with
-            | AI_Random -> getAIInputForEntity cc
-            | Keyboard -> getAIInputForEntity cc
-        match newCC = cc with
-        | true -> m
-        | false -> m.Remove(newCC.ID).Add(newCC.ID,Controller newCC)
-        ) ent.Components
+    |> Array.Parallel.map ToController
+    |> Array.Parallel.map (getCurrentActions ent round)
+    |> Array.Parallel.partition (fun c -> c.ControllerType = Keyboard)
+    |> handleSplitInputTypes
 
 
 
+
+//|> Array.Parallel.map (fun (Controller c) -> setCurrentActions c) 
+//|> Array.fold (fun (m:Map<ComponentID,Component>) (cc:ControllerComponent) ->
+//    let newCC = getInputForEntity cc
+//    match newCC = cc with
+//    | true -> m
+//    | false -> m.Remove(newCC.ID).Add(newCC.ID,Controller newCC)
+//    ) ent.Components
 
 //let updateActionIfChanged (controller:ControllerComponent) newAction = 
 //    if (newAction <> controller.CurrentAction) then
@@ -88,3 +100,25 @@ let getInputForAllEntities (ent:Entities) (*log:agent_GameLog*) (round:RoundNumb
     //|> Array.map getKeyboardInputForEntity
     //|> Array.forall (fun b -> b)
 
+//let getKeyboardInputForEntity (cc:ControllerComponent) =
+//    {
+//        cc with
+//            CurrentAction = 
+//                match cc.ControllerType with
+//                | Keyboard -> 
+//                    awaitKeyboardInput enm controller renderer round
+//                | _ -> Idle // Should raise an error?
+//    }
+//    let newAction,cont =
+//        match controller.ControllerType with
+//        | Keyboard -> 
+//            AwaitKeyboardInput enm controller renderer round
+//        | _ -> Idle,false // Should raise an error
+//    match cont with
+//    | false -> false
+//    | true -> 
+//        updateActionIfChanged controller newAction
+//        //if (newAction <> controller.CurrentAction) then
+//        //    enm.UpdateComponent (Controller { controller with CurrentAction = newAction })
+//        //    log.Log round (sprintf "%-3s | %-20s -> %-30s #%7i.%i : %A" "Ok" "Controller System" "Current action" controller.EntityID.ToUint32 controller.ID.ToUint32 newAction)
+//        true
---
+++ W2F/Systems/Entities.fs
@@ -0,0 +1,98 @@
+﻿module Entities
+open CommonFunctions
+open CommonTypes
+open Component
+open ComponentEnums
+open FormComponent
+
+type Entities = 
+    {
+        Components : Map<ComponentID,Component>
+        ComponentTypes : Map<ComponentTypes,ComponentID[]>
+        Entities : Map<EntityID,ComponentID[]>
+        Locations : Map<Location,ComponentID[]>
+        MaxComponentID : ComponentID
+        MaxEntityID : EntityID
+    }
+    static member empty = 
+        {
+            Components = Map.empty
+            ComponentTypes = Map.empty
+            Entities = Map.empty
+            Locations = Map.empty
+            MaxComponentID = ComponentID(0u)
+            MaxEntityID = EntityID(0u)
+        }
+
+let private addComponents (start:Map<ComponentID,Component>) (cts:Component[]) =
+    cts
+    |> Array.fold (fun (m:Map<ComponentID,Component>) (c:Component) -> 
+        m.Add(getComponentID c,c)
+        ) start
+
+let private addComponentTypes (start:Map<ComponentTypes,ComponentID[]>) (cts:Component[]) =
+    cts
+    |> Array.fold (fun (m:Map<ComponentTypes,ComponentID[]>) (c:Component) ->
+        map_AppendValueToArrayUnique m (getComponentType c) (getComponentID c)
+        ) start
+
+let private addLocation (start:Map<Location,ComponentID[]>) (cts:Component[]) = 
+    cts 
+    |> Array.filter (fun (c:Component) -> getComponentType c = FormComponent)
+    |> Array.Parallel.map ToForm
+    |> Array.fold (fun (m:Map<Location,ComponentID[]>) (f:FormComponent) ->
+        map_AppendValueToArrayUnique m f.Location f.ID
+        ) start
+
+let private moveLocation (start:Map<Location,ComponentID[]>) (cts:Component[]) = 
+    cts 
+    |> Array.filter (fun (c:Component) -> getComponentType c = FormComponent)
+    |> Array.Parallel.map ToForm
+    |> Array.fold (fun (m:Map<Location,ComponentID[]>) (f:FormComponent) ->
+        //get old form
+        //check if location is the same
+        //move if needed
+        map_AppendValueToArrayUnique m f.Location f.ID
+        ) start
+
+let createEntity (ent:Entities) (cts:Component[]) = 
+    {
+        Components = addComponents ent.Components cts
+        ComponentTypes = addComponentTypes ent.ComponentTypes cts
+        Entities = ent.Entities.Add(ent.MaxEntityID + 1u,cts |> Array.map getComponentID)
+        Locations = addLocation ent.Locations cts
+        MaxComponentID = ent.MaxComponentID + ComponentID(uint32 cts.Length)
+        MaxEntityID = ent.MaxEntityID + 1u
+    }
+
+let getComponent_ByID (ent:Entities) cid = ent.Components.Item cid
+
+let getComponents_OfType (ent:Entities) ct =
+    ent.ComponentTypes.Item ct
+    |> Array.map (getComponent_ByID ent)
+
+let getEntity (ent:Entities) (eid:EntityID) = 
+    match (ent.Entities.ContainsKey eid) with
+    | false -> [||]
+    | true -> ent.Entities.Item eid
+
+let getLocationMap (ent:Entities) = 
+    ent.Locations
+    |> Map.map (fun _ cids -> 
+        cids
+        |> Array.map (getComponent_ByID ent)
+        |> Array.map ToForm
+        )
+    
+let updateComponents (ent:Entities) (cts:Component[]) = 
+    {
+        ent with
+            Components = 
+                cts
+                |> Array.fold (fun (m:Map<ComponentID,Component>) (c:Component) -> 
+                    m.Remove(getComponentID c).Add(getComponentID c,c)
+                    ) ent.Components
+            Locations =
+                ent.Locations
+    }
+
